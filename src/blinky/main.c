4c7f6c1620f4c2b4ca2ed5e426a34b1dba4050fb
masonchester <110614975+masonchester@users.noreply.github.com>
2026-05-31 10:17:47 -0400

    Add stm32l4xx CMSIS headers

---
+++ src/blinky/main.c
@@ -0,0 +1,26 @@
+#include "stm32l476xx.h"
+
+#define LED_PIN 5
+
+void delay(int ms){
+    for(volatile int i = 0; i <= ms; i++){}
+}
+
+int main(){
+
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+
+    GPIOA->MODER &= ~(3 << (LED_PIN * 2));
+    GPIOA->MODER |= (1 << (LED_PIN * 2));
+
+    while (1) {
+        
+        GPIOA->BSRR |= (1 << LED_PIN);
+        delay(10000);
+
+        GPIOA->BSRR |= (1 << (LED_PIN + 16));
+        delay(10000);
+    }
+
+    return 0;
+}
